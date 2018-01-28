59ae5bc77ca29b9ffd4ec9055afedb53dbd93015
yudonguk <yudonguk@gmail.com>
2018-01-29 03:05:14 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,15 +2,15 @@
 DOCKER_REPOSITORY=yudonguk/bicomc-docker
 DOCKER_TAGS=`wget -qO - https://registry.hub.docker.com/v1/repositories/$DOCKER_REPOSITORY/tags | grep -Eo '(?:"name"\s*:\s*)"[^"]+"' | grep -Eo '"[^"]+"$' | grep -Eo '[^"]+'`
 
-mkdir -p ~/docker
+mkdir -p docker
 #docker pull -a $DOCKER_REPOSITORY || exit 1;
 
 for tag in $DOCKER_TAGS; do
   echo; echo;
   echo -e "\x1B[34m-- Compiler: $tag\x1B[0m";
   
-  if [[ -f ~/docker/$tag.tar ]]; then
-    docker load -i ~/docker/$tag.tar;
+  if [[ -f docker/$tag.tar ]]; then
+    docker load -i docker/$tag.tar;
   fi
 
   sourceDir="/root/`basename $PWD`";
@@ -24,5 +24,5 @@ for tag in $DOCKER_TAGS; do
     exit 1;
   fi
 
-  docker save -o ~/docker/$tag.tar $DOCKER_REPOSITORY:$tag;
+  docker save -o docker/$tag.tar $DOCKER_REPOSITORY:$tag;
 done
