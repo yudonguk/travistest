92d75bec14927c0115299b39ffa3402b9b09d66b
yudonguk <yudonguk@gmail.com>
2018-01-29 02:20:00 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -8,6 +8,9 @@ fi
 
 docker pull -a $DOCKER_REPOSITORY || exit 1;
 
+mkdir -p ~/docker
+docker save $DOCKER_REPOSITORY > ~/docker/image.tar
+
 for tag in $DOCKER_TAGS; do
   echo; echo;
   echo -e "\x1B[34m-- Compiler: $tag\x1B[0m";
@@ -17,12 +20,9 @@ for tag in $DOCKER_TAGS; do
 
   mkdir -p "$buildDir";
 
-  buildCmd="cmake ../.. && cmake --build . && cd example/simple_example && ./simple_example";
+  buildCmd="cmake ../.. && cmake --build .;
   docker run --rm -v "$PWD":"$sourceDir" -w "$sourceDir/$buildDir" $DOCKER_REPOSITORY:$tag bash -c "$buildCmd";
   if [ $? -ne 0 ]; then
     exit 1
   fi
 done
-
-mkdir -p ~/docker
-docker save $DOCKER_REPOSITORY > ~/docker/image.tar
